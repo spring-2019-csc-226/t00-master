a5a16aa40d5c26e3da40958e6d05ef39601de6fd
Emily Lovell <lovelle@berea.edu>
2019-01-13 22:06:19 -0500

    Add .gitignore and tidy starter code

--- t00_chocolate.py
+++ t00_chocolate.py
@@ -1,20 +1,20 @@
 ######################################################################
-# Author: Dr. Scott Heggen           ****** TODO: CHANGE THIS!! ******
-# username: heggens             ****** TODO: CHANGE THIS!! *****
+# Author: Emily Lovell          ****** TODO: CHANGE THIS!! ******
+# username: lovelle             ****** TODO: CHANGE THIS!! *****
 #
-# Purpose: Designed to compute the total chocolate desired by the user
+# Purpose: Compute the total chocolate desired by the user
 #
 ######################################################################
 # Acknowledgements:
 #
-# Modified from original code written by Dr. Jan Pearce
+# Modified from original code written by Dr. Jan Pearce & Dr. Scott Heggen
 #
 # Licensed under a Creative Commons
 # Attribution-Noncommercial-Share Alike 3.0 United States License.
 ######################################################################
 
 
-# Section 1: User Input Section
+# Section 1: User Input
 print("")
 entered_name = input("Please enter your name: ")
 print("Hello " + entered_name + "! \n")
@@ -23,20 +23,20 @@ num_boxes = int(input("How many chocolate boxes you would like? "))
 num_lbs = int(input("How many lbs of chocolate in a box? \n"))
 
 
-# Section 2: Computation Section
+# Section 2: Computation
 lbs_choc = num_boxes * num_lbs
 oz_per_lb = 16
 oz_choc = lbs_choc * oz_per_lb
 
 
-# Section 3: Conditional (Decision-making) Section
+# Section 3: Conditional (Decision-making)
 if oz_choc > 500:
     print("Wow, you must really like chocolate!")
 else:
     print("Not a choc-o-holic, I guess! ")
 
-# Section 4: Displaying Results Section
+# Section 4: Displaying Results
 print(str(num_boxes) + " boxes\nwith " + str(num_lbs) + " lbs of chocolate per box\nis" + " " + str(oz_choc) + " ounces of chocolate.\n" )
 print("Studies have shown there are health benefits from eating 1.5 to 3 ounces of dark chocolate daily!")
 
-# To run this code, hit the green run button.
+# To run this code, hit the green run button
